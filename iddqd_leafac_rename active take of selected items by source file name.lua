d5e91d66be47abc3e9f6af1df4ff8d9b0802f0a3
iddqdsound <86320208+iddqdsound@users.noreply.github.com>
2021-06-22 14:29:50 -0400

    v0
    somehow I'm still alive...

---
+++ iddqd_leafac_rename active take of selected items by source file name.lua
@@ -0,0 +1,20 @@
+
+
+local SelectedItemCount = reaper.CountSelectedMediaItems(0)
+if SelectedItemCount == 0 then 
+    reaper.MB("Please make an item selection!","Error",0) 
+    return 
+end 
+for SelectedItemIndex = 0,SelectedItemCount-1 do
+    local SelectedMediaItem = reaper.GetSelectedMediaItem(0, SelectedItemIndex) --returns the first selected item, 1st 0 is active project, --TODO--
+    local ActiveTakeInItem = reaper.GetActiveTake(SelectedMediaItem) --Returns the active take in media item
+    local SelectedTakeSource = reaper.GetMediaItemTake_Source(ActiveTakeInItem) --Returns the Source File of active take
+    local SelectedTakeSourceName = reaper.GetMediaSourceFileName(SelectedTakeSource,"") --returns the full path of source file name
+    local SourceBaseName = string.gsub(SelectedTakeSourceName,".*[/\\]","") --Removes the path name from the source file name
+       reaper.GetSetMediaItemTakeInfo_String(ActiveTakeInItem,"P_NAME",SourceBaseName,true) --renames the active take name by the above variablereaper.GetSetMediaItemTakeInfo_String(ActiveTakeInItem,"P_NAME",SourceBaseName,true) --renames the active take name by the above variable
+
+end   
+
+
+
+
