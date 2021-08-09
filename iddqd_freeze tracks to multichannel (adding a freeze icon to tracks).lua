0baa75632a68df8b5ebc50a038625fd4511be27d
iddqdsound <86320208+iddqdsound@users.noreply.github.com>
2021-08-09 19:09:48 -0400

    Freeze Actions Update
    Added Undo block

--- iddqd_freeze tracks to multichannel (adding a freeze icon to tracks).lua
+++ iddqd_freeze tracks to multichannel (adding a freeze icon to tracks).lua
@@ -5,5 +5,19 @@
  * If your slot 1 is occupied, change the number in front of the action below (in pink) to the slot number your freeze icon is on
  * Version: 1.0
 ]]--
-reaper.Main_OnCommand(40877,0)
-reaper.Main_OnCommand(reaper.NamedCommandLookup("_S&M_SET_TRACK_ICON1"),0)
+
+local function main()
+  reaper.PreventUIRefresh(1)
+  reaper.Undo_BeginBlock()
+
+  reaper.Main_OnCommand(40877,0)
+  reaper.Main_OnCommand(reaper.NamedCommandLookup("_S&M_SET_TRACK_ICON1"),0)
+
+  reaper.Undo_EndBlock('Freeze tracks to multichannel', 0)
+  reaper.PreventUIRefresh(-1)
+  reaper.UpdateArrange()
+  reaper.UpdateTimeline()
+end
+
+main()
+
--- iddqd_unfreeze tracks (removing frozen icon from tracks).lua
+++ iddqd_unfreeze tracks (removing frozen icon from tracks).lua
@@ -5,5 +5,17 @@
  * This action only makes sense if you have the frozen track icon set up
  * Version: 1.0
 ]]--
-reaper.Main_OnCommand(41644,0)
-reaper.Main_OnCommand(40900,0)
+local function main()
+  reaper.PreventUIRefresh(1)
+  reaper.Undo_BeginBlock()
+
+  reaper.Main_OnCommand(41644,0)
+  reaper.Main_OnCommand(40900,0)
+
+  reaper.Undo_EndBlock('Freeze tracks to mono', 0)
+  reaper.PreventUIRefresh(-1)
+  reaper.UpdateArrange()
+  reaper.UpdateTimeline()
+end
+
+main()
