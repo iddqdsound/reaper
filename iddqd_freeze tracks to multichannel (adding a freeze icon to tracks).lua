4840c373914f5bb8dff4a5d98a14353c988da97a
iddqdsound <86320208+iddqdsound@users.noreply.github.com>
2021-08-09 05:38:11 -0400

    freeze actions
    with icon!

---
+++ iddqd_freeze tracks to multichannel (adding a freeze icon to tracks).lua
@@ -0,0 +1,9 @@
+--[[
+ * ReaScript Name: Freeze to multichannel (adding a freeze Icon to Frozen Tracks
+ * Author: IDDQD
+ * Instruction: Find and place the freeze.png icon in your SWS Resources>Images>Slot 1
+ * If your slot 1 is occupied, change the number in front of the action below (in pink) to the slot number your freeze icon is on
+ * Version: 1.0
+]]--
+reaper.Main_OnCommand(40877,0)
+reaper.Main_OnCommand(reaper.NamedCommandLookup("_S&M_SET_TRACK_ICON1"),0)
---
+++ iddqd_unfreeze tracks (removing frozen icon from tracks).lua
@@ -0,0 +1,9 @@
+--[[
+ * ReaScript Name: Unfreeze Tracks (removing frozen icon)
+ * Author: IDDQD
+ * Instruction: Find and place the freeze.png icon in your SWS Resources>Images>Slot 1
+ * This action only makes sense if you have the frozen track icon set up
+ * Version: 1.0
+]]--
+reaper.Main_OnCommand(41644,0)
+reaper.Main_OnCommand(40900,0)
